e5a42ed8bdd277796e69f69a054e806622f232ac
CodeWithMarcelo <54737417+razorrazo@users.noreply.github.com>
2022-11-06 05:28:41 -0400

    Este repositorio se hizo utilizando un fresh setup de laravel, primero creamos un middleware con el comando artisan llamado Localization e incorporamos un codigo para localizar la session del usuario. Luego abrimos el kernel de Laravel y dentro de middlewaregroups agregamos la ubicacion del middleware Localization que creamos. En el archivo de rutas web.php agreamos la ruta de locale para que pueda cambiar el usuario de idioma con un selector que agregamos en la vista de navigation.blade.php la cual detnro su href. Previamente debemos instalar el paquete de idioma de Laravel llamado laravel lang o agregarlas manualmente en la carpeta de lang el video que me ayudo en este procesos fue https://www.youtube.com/watch?v=TDF7I3X5GuQ

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('locale/{locale}', function ($locale) {
+    Session::put('locale',$locale);
+    return redirect()->back();
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
